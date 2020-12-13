91cb50af1017b0555b92570753fb31f8f4677cad
Wairimu Mungai <nimowairimu25@gmail.com>
2020-12-13 15:04:25 +0300

    Add month validator.

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,21 @@ var femaleAkanNames=["Akosua","Adwoa ","Abenaa", "Akua ","Yaa","Afua","Ama"];
 // Array for days of the week
 var daysOfTheWeek=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 // Validate date
+function dayValidator() {
 if (day<=0 || day>=32) {
   return false;
 }else {
   return true
+  }
 }
+//Validate monthOfBirth
+function monthValidator () {
+  if (monthOfBirth < 1 || monthOfBirth > 12) {
+    return false;
+  } else {
+    return true;
+  }
+}
+  //validation variables
+  let monthValid = monthValidator();
+  let dayValid = dayValidator();
