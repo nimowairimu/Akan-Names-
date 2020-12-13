53923f590e4e088ae3df37d98975be3e8fc18da3
Wairimu Mungai <nimowairimu25@gmail.com>
2020-12-13 14:34:36 +0300

    Array for days of the week.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,5 @@
 // Array for my name
 var maleAkanNames= ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleAkanNames=["Akosua","Adwoa ","Abenaa", "Akua ","Yaa","Afua","Ama"];
+// Array for days of the week
+var daysOfTheWeek=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
