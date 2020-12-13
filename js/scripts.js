150db73faa3ffadbb05a626a0c76d16f95d549a4
Wairimu Mungai <nimowairimu25@gmail.com>
2020-12-13 14:52:29 +0300

    Add day validator.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@ var maleAkanNames= ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleAkanNames=["Akosua","Adwoa ","Abenaa", "Akua ","Yaa","Afua","Ama"];
 // Array for days of the week
 var daysOfTheWeek=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// Validate date
+if (day<=0 || day>=32) {
+  return false;
+}else {
+  return true
+}
