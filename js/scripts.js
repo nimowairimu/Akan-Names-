7375da1efc8ca0db3b02c89c5ce8421f5a21326a
Wairimu Mungai <nimowairimu25@gmail.com>
2020-12-14 15:07:05 +0300

    make final changes and alignment.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,81 @@
-// Array for my name
-var maleAkanNames= ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var femaleAkanNames=["Akosua","Adwoa ","Abenaa", "Akua ","Yaa","Afua","Ama"];
-// Array for days of the week
-var daysOfTheWeek=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-// Validate date
-function dayValidator() {
-if (day<=0 || day>=32) {
-  return false;
-}else {
-  return true
-  }
+//Arrays
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Afua","Afua","Ama"];
+
+//functions
+
+var day = function(){
+    century = parseInt(document.getElementById("century").value);
+    year = parseInt(document.getElementById("year").value);
+    month = parseInt(document.getElementById("month").value);
+    date = parseInt(document.getElementById("date").value);
 }
-//Validate monthOfBirth
-function monthValidator () {
-  if (monthOfBirth < 1 || monthOfBirth > 12) {
-    return false;
-  } else {
-    return true;
-  }
+
+function calculate(){
+    day();
+    dayOfTheWeek = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
+    return(Math.floor(dayOfTheWeek));
+    console.log(dayOfTheWeek);
 }
-  //validation variables
-  let monthValid = monthValidator();
-  let dayValid = dayValidator();
+
+var result = function(){
+dayOfTheWeek = calculate();
+checkgender();
+}
+
+var checkgender = function() {
+    calculate();
+    var Gender = document.getElementsByName;
+    if(gender[0].checked == true){
+        gender = "male";
+    }else if(gender[1].checked == true ){
+        gender = "female";
+    }
+
+    switch(gender){
+        case "male":
+          if(dayOfTheWeek == 0){
+            document.getElementById("result").innerHTML = "Sunday" + MaleNames[0];
+
+           }else if (dayOfTheWeek == 1){
+            document.getElementById("result").innerHTML = "Monday" + maleNames[1];
+           }else if (dayOfTheWeek == 2){
+            document.getElementById("result").innerHTML = "Tuesday" + maleNames[2];
+           }else if (dayOfTheWeek == 3){
+            document.getElementById("result").innerHTML = "Wednesday" + maleNames[3];
+           }else if (dayOfTheWeek == 4){
+            document.getElementById("result").innerHTML = "Thursday" + maleNames[4];
+           }else if (dayOfTheWeek == 5){
+            document.getElementById("result").innerHTML  = "Friday" + maleNames[5];
+           }else {
+            document.getElementById("result").innerHTML = 'Saturday' + maleNames[6];
+           }
+		   break;
+        case "female":
+            if(dayOfTheWeek == 0){
+                document.getElementById("result").innerHTML = "Sunday" + femaleNames[0];
+                console.log("Your akan name is  akosua")
+            }else if (dayOfTheWeek == 1){
+                document.getElementById("result").innerHTML = "Monday" + femaleNames[1];
+                console.log ("Your akan name is adwoa ")
+            }else if (dayOfTheWeek == 2){
+                document.getElementById("result").innerHTML = "Tuesday" + femaleNames[2];
+                console.log("Your akan name is abenaa")
+            }else if (dayOfTheWeek == 3){
+                document.getElementById("result").innerHTML = "Wednesday" + femaleNames[3];
+                console.log ("Your akan name is akua")
+            }else if (dayOfTheWeek == 4){
+                document.getElementById("result").innerHTML = "Thursday" + femaleNames[4];
+                console.log ("Your akan name is yaa")
+            }else if (dayOfTheWeek == 5){
+                document.getElementById("result").innerHTML = "Friday" + femaleNames[5];
+                console.log("Your akan name is afua")
+            }else{
+                document.getElementById("result").innerHTML = "Saturday" + femaleNames[6];
+                console.log("Your akan name is ama")
+            }
+			break;
+        default:
+         console.log(result);
+	}
+}
