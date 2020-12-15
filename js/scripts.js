a9f2fa1200793665a262db8a4ff183fe5680c12c
Wairimu Mungai <nimowairimu25@gmail.com>
2020-12-15 14:18:09 +0300

    Make changes to script.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //Arrays
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Afua","Afua","Ama"];
-
+var dayOfTheWeek =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 var day = function(){
     century = parseInt(document.getElementById("century").value);
@@ -15,6 +15,16 @@ function calculate(){
     dayOfTheWeek = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
     return(Math.floor(dayOfTheWeek));
     console.log(dayOfTheWeek);
+}
+
+if (Month < 1 || Month> 12) {
+    alert("Incorrect month.")
+    return;
+}
+
+if (Date< 0 ||Date >31 ){
+  alert( "Incorrect date.")
+  return;
 }
 
 var result = function(){
@@ -35,19 +45,25 @@ var checkgender = function() {
         case "male":
           if(dayOfTheWeek == 0){
             document.getElementById("result").innerHTML = "Sunday" + MaleNames[0];
-
+            console log ("Your akan name is Kwasi")
            }else if (dayOfTheWeek == 1){
             document.getElementById("result").innerHTML = "Monday" + maleNames[1];
+            console log ("Your akan name is Kwadwo")
            }else if (dayOfTheWeek == 2){
             document.getElementById("result").innerHTML = "Tuesday" + maleNames[2];
+            console log ("Your akan name is Kwabena")
            }else if (dayOfTheWeek == 3){
             document.getElementById("result").innerHTML = "Wednesday" + maleNames[3];
+            console log ("Your akan name is Kwaku")
            }else if (dayOfTheWeek == 4){
             document.getElementById("result").innerHTML = "Thursday" + maleNames[4];
+            console log ("Your akan name is Yaw")
            }else if (dayOfTheWeek == 5){
             document.getElementById("result").innerHTML  = "Friday" + maleNames[5];
+            console log ("Your akan name is Kofi")
            }else {
             document.getElementById("result").innerHTML = 'Saturday' + maleNames[6];
+            console log ("Your akan name is Kwame ")
            }
 		   break;
         case "female":
